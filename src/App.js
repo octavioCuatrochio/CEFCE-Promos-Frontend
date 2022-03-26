0400cb2a1b335bb7299ae295e68496b08cb5456a
Octavio <octaviocuatrochio45545mail.com>
2022-03-25 22:49:48 -0300

    call to api on front-end

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// import { useState } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 import { Route, Switch, Redirect } from "react-router-dom";
 import './App.css';
 import Container from "./components/UI/Container";
@@ -11,126 +11,95 @@ import AdminContainer from "./components/Admin/AdminContainer";
 
 function App() {
 
-  let PROMOS_PLACEHOLDER = [
-    {
-      id: 0,
-      dia: "lunes",
-      nombre: "Descuento Antares",
-      lugar: "Av. España 2254",
-      tipo: "cefce",
-      imagen: null,
-      descripcion: "Descuento en birras y papas toda la noche de un 30%."
-    },
-    {
-      id: 2,
-      dia: "lunes",
-      nombre: "promo Tandilia",
-      lugar: "calle Brasil 123",
-      tipo: "cefce",
-      imagen: null,
-      descripcion: "una promo de papas."
-    },
-    {
-      id: 3,
-      dia: "martes",
-      nombre: "promo",
-      lugar: "calle 123",
-      tipo: "facultad",
-      imagen: null,
-      descripcion: "una promo."
-    },
-    {
-      id: 0,
-      dia: "lunes",
-      nombre: "Descuento Antares",
-      lugar: "Av. España 2254",
-      tipo: "facultad",
-      imagen: null,
-      descripcion: "Descuento en birras y papas toda la noche de un 30%."
-    }
-  ]
+  // let PROMOS_PLACEHOLDER = [
+  //   {
+  //     id: 0,
+  //     dia: "lunes",
+  //     nombre: "Descuento Antares",
+  //     lugar: "Av. España 2254",
+  //     tipo: "cefce",
+  //     imagen: null,
+  //     descripcion: "Descuento en birras y papas toda la noche de un 30%."
+  //   },
+  //   {
+  //     id: 2,
+  //     dia: "lunes",
+  //     nombre: "promo Tandilia",
+  //     lugar: "calle Brasil 123",
+  //     tipo: "cefce",
+  //     imagen: null,
+  //     descripcion: "una promo de papas."
+  //   },
+  //   {
+  //     id: 3,
+  //     dia: "martes",
+  //     nombre: "promo",
+  //     lugar: "calle 123",
+  //     tipo: "facultad",
+  //     imagen: null,
+  //     descripcion: "una promo."
+  //   },
+  //   {
+  //     id: 0,
+  //     dia: "lunes",
+  //     nombre: "Descuento Antares",
+  //     lugar: "Av. España 2254",
+  //     tipo: "facultad",
+  //     imagen: null,
+  //     descripcion: "Descuento en birras y papas toda la noche de un 30%."
+  //   }
+  // ]
 
-  // const [promos, setPromos] = useState(PROMOS_PLACEHOLDER);
-
-  let cefcePromos = [
-    {
-      dia: "Lunes",
-      id: 0,
-      promos: []
-    },
-    {
-      dia: "Martes",
-      id: 1,
-      promos: []
-    },
-    {
-      dia: "Miércoles",
-      id: 2,
-      promos: []
-    },
-    {
-      dia: "Jueves",
-      id: 3,
-      promos: []
-    },
-    {
-      dia: "Viernes",
-      id: 4,
-      promos: []
-    },
-    {
-      dia: "Sábado",
-      id: 5,
-      promos: []
-    },
-    {
-      dia: "Domingo",
-      id: 6,
-      promos: []
-    }
-  ];
-  let uniPromos = [];
+  const [promos, setPromos] = useState([]);
+  const [cefceListPromos, setCefceListPromos] = useState([
+  ]);
+  const [uniListPromos, setUniListPromos] = useState([]);
   // const [error, setError] = useState(false);
   // const [isLoading, setIsLoading] = useState(false);
 
+  const filterPromos = useCallback(() => {
 
-  // const fetchPromosHandler = useCallback(async () => {
-  //   // setIsLoading(true);
-  //   // setError(null);
-  //   try {
-  //     const response = await fetch('https://cefcepromoapi.000webhostapp.com/api/promos');
-  //     if (!response.ok) {
-  //       throw new Error('Something went wrong!');
-  //     }
+    let cefcePromos = [
+      {
+        dia: "Lunes",
+        id: 0,
+        promos: []
+      },
+      {
+        dia: "Martes",
+        id: 1,
+        promos: []
+      },
+      {
+        dia: "Miércoles",
+        id: 2,
+        promos: []
+      },
+      {
+        dia: "Jueves",
+        id: 3,
+        promos: []
+      },
+      {
+        dia: "Viernes",
+        id: 4,
+        promos: []
+      },
+      {
+        dia: "Sábado",
+        id: 5,
+        promos: []
+      },
+      {
+        dia: "Domingo",
+        id: 6,
+        promos: []
+      }
+    ];
 
-  //     const data = await response.json();
+    let uniPromos = [];
 
-  //     const loadedPromos = [];
-
-  //     console.log(data);
-
-  //     // for (const key in data) {
-  //     //   loadedMovies.push({
-  //     //     id: key,
-  //     //     title: data[key].title,
-  //     //     openingText: data[key].openingText,
-  //     //     releaseDate: data[key].releaseDate
-  //     //   })
-  //     // }
-
-  //     // setMovies(loadedMovies);
-  //   } catch (error) {
-  //     // setError(error.message);
-  //   }
-  //   // setIsLoading(false);
-  // }, []);
-
-  // useEffect(() => {
-  //   fetchPromosHandler();
-  // }, []);
-
-  function filterPromos(newPromos) {
-    for (let auxPromo of newPromos) {
+    for (let auxPromo of promos) {
 
       if (auxPromo.tipo === "cefce") {
         switch (auxPromo.dia) {
@@ -158,13 +127,39 @@ function App() {
           default:
             console.log("algo salió mal");
         }
-
       } else {
         uniPromos.push(auxPromo);
       }
     }
-  }
-  filterPromos(PROMOS_PLACEHOLDER);
+    setCefceListPromos(cefcePromos);
+    setUniListPromos(uniPromos);
+  }, [promos]);
+
+
+  const fetchPromosHandler = useCallback(async () => {
+    // setIsLoading(true);
+    // setError(null);
+    try {
+      const response = await fetch('https://cefcepromoapi.000webhostapp.com/api/promos');
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+      const data = await response.json();
+
+      setPromos(data);
+      filterPromos();
+
+    } catch (error) {
+      console.log(error.message);
+    }
+    // setIsLoading(false);
+  }, [filterPromos]);
+
+
+  useEffect(() => {
+    fetchPromosHandler();
+  }, []);
+
 
   return (
     <Switch>
@@ -183,10 +178,10 @@ function App() {
               <Redirect to="/cefce" />
             </Route>
             <Route path="/cefce">
-              <PromoContainer items={cefcePromos} />
+              <PromoContainer items={cefceListPromos} />
             </Route>
             <Route path="/facultad">
-              <PromoList items={uniPromos} />
+              <PromoList items={uniListPromos} />
             </Route>
           </Switch>
         </Container>
